4d8857290e30bd3a7d71f62717832b0d2a874a35
Saloni Bharti <salonibharti728@gmail.com>
2026-09-09 18:03:39 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+import(
+  "fmt"
+  "net/http"
+)
+func main() {
+  http.HandleFunc("/", func(w http.ResponseWriter, r "http.Resquest){
+      fmt.Fprint(w,"Hello from Google app engine - Go!")
+  })
+    http.ListenAndServer(":8000",nil)
+  }
+  
+                  
