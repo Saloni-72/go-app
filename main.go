2b50dc4f2259aa0c23c38ca6637742edc7768cf6
Saloni Bharti <salonibharti728@gmail.com>
2026-09-09 18:21:16 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import(
   "net/http"
 )
 func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r "http.Resquest){
+  http.HandleFunc("/", func(w http.ResponseWriter, r*http.Resquest){
       fmt.Fprint(w,"Hello from Google app engine - Go!")
 })
     http.ListenAndServe(":8080",nil)
